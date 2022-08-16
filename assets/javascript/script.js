1f74cf840badd6dfdff62799b0a5d9993c941e7a
ckrum21 <108544020+ckrum21@users.noreply.github.com>
2022-08-16 16:31:58 -0400

    local storage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,24 @@
-
-
 // handle displaying the time
 var timeDisplayEl = moment();
 $("#currentDay").text(timeDisplayEl.format("MMMM Do, YYYY"));
 
+var task = document.querySelector(".task");
+var button = document.querySelector(".button");
+
+function renderLastRegistered() {
+    var task = localStorage.getItem("task");
+      
+    task.textContent = task;
+  }
+  
+
+button.addEventListener("click", function(event) {
+    event.preventDefault();
+  
+    var task = document.querySelector(".task").value;
+
+      localStorage.setItem("task", task);
+      renderLastRegistered();
+    }
+  );
+  
