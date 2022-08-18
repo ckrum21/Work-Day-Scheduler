87652abd1b6056ca3d1d5ba03330e0298da206ec
ckrum21 <108544020+ckrum21@users.noreply.github.com>
2022-08-18 13:05:26 -0400

    color time blocks

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,24 +1,54 @@
-// handle displaying the time
+
+
 var timeDisplayEl = moment();
 $("#currentDay").text(timeDisplayEl.format("MMMM Do, YYYY"));
 
-var task = document.querySelector(".task");
-var button = document.querySelector(".button");
+$(document).ready(function () {
+  $(".saveBtn").on("click", function () {
+  
+      var text = $(this).siblings(".task").val();
+      var time = $(this).parent().attr("id");
 
-function renderLastRegistered() {
-    var task = localStorage.getItem("task");
+
+      localStorage.setItem(time, text);
+  })
+ 
+  function timeTracker() {
+    
+      var timeNow = moment().hour();
+    
+      $(".time-block").each(function () {
+          var blockTime = parseInt($(this).attr("id"));
+
       
-    task.textContent = task;
+          if (blockTime < timeNow) {
+              $(this).removeClass("future");
+              $(this).removeClass("present");
+              $(this).addClass("past");
+          }
+          else if (blockTime === timeNow) {
+              $(this).removeClass("past");
+              $(this).removeClass("future");
+              $(this).addClass("present");
+          }
+          else {
+              $(this).removeClass("present");
+              $(this).removeClass("past");
+              $(this).addClass("future");
+
+          }
+      })
   }
-  
 
-button.addEventListener("click", function(event) {
-    event.preventDefault();
-  
-    var task = document.querySelector(".task").value;
+  $("#hour9 .task").val(localStorage.getItem("hour9"));
+  $("#hour10 .task").val(localStorage.getItem("hour10"));
+  $("#hour11 .task").val(localStorage.getItem("hour11"));
+  $("#hour12 .task").val(localStorage.getItem("hour12"));
+  $("#hour1 .task").val(localStorage.getItem("hour1"));
+  $("#hour2 .task").val(localStorage.getItem("hour2"));
+  $("#hour3 .task").val(localStorage.getItem("hour3"));
+  $("#hour4 .task").val(localStorage.getItem("hour4"));
+  $("#hour5 .task").val(localStorage.getItem("hour5"));
 
-      localStorage.setItem("task", task);
-      renderLastRegistered();
-    }
-  );
-  
+ timeTracker();
+}) 
